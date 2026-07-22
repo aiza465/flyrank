55fffa70e822ea651c30e5dbe9a3618cd469d88c
aiza nabi <aizanabi44@gmail.com>
2026-07-22 20:22:06 +0500

    Fix Stage 2: correct GET /tasks/{id} logic

--- WEEK2.py
+++ WEEK2.py
@@ -23,10 +23,12 @@ tasks = [
 @app.get("/tasks")
 async def get_tasks():
     return tasks
+
 @app.get("/tasks/{id}")
 async def get_task(id: int):
     for task in tasks:
         if task["id"] == id:
             return task
-        else :
-            raise HTTPException(status_code=404, detail="Task not found")
+    else :
+        raise HTTPException(status_code=404, detail="Task not found")
+
